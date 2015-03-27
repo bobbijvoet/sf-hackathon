544be407c37a8c727afe2a567e7474203118c8b0
Bob <bob@frontmen.nl>
2015-03-27 13:26:59 +0100

    Route to marker detail

--- src/scripts/modules/map/MapCtrl.js
+++ src/scripts/modules/map/MapCtrl.js
@@ -2,25 +2,39 @@
  * Created by bob on 27-3-15.
  */
 class MapCtrl {
-  constructor (){
-    console.log('constructor');
-    var map = L.map('map', {
-        center: [51.505, -0.09],
-        zoom: 13
+
+  constructor($state) {
+    this.map = L.map('map', {
+      center: [51.505, -0.09],
+      zoom: 13
     });
 
-    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 18
-    }).addTo(map);
+    this.$state = $state;
 
-    var marker = L.marker([51.5, -0.09]).addTo(map);
+    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 18
+    }).addTo(this.map);
+
+    this.marker = L.marker([51.5, -0.09], {id: 123}).addTo(this.map);
+
+    this.marker.on('click', (event) => this.$state.go('marker', {id:event.target.options.id}));
+
   }
 
 
-  showPopup(){
-    console.log("YES");
+  showPopup(event) {
+
+    //L.popup()
+    //  .setLatLng([51.5, -0.09])
+    //  .setContent(`<p>${event.target.options.id}<br /></p>`)
+    //  .openOn(this.map);
+
+    this.$state.go('marker', {id:event.target.options.id})
 
   }
 }
+
+MapCtrl.$inject = ['$state'];
+
 
 export default MapCtrl;
