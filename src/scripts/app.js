1a998979a2ff8abc23879bbf6a1e20c238923268
Lucas Calje <lucas@frontmen.nl>
2015-03-27 10:53:04 +0100

    fixed errors

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,4 +1,4 @@
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+angular.module('starter', ['ionic'])
 
   .run(($ionicPlatform)=> {
     $ionicPlatform.ready(function () {
