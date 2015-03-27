e6772e3a8cc59caa701436634eb499c9de57fcfa
Bob <bob@frontmen.nl>
2015-03-27 12:30:26 +0100

    Gulp watch scss, fullscreen map with marker

--- src/scripts/modules/map/MapCtrl.js
+++ src/scripts/modules/map/MapCtrl.js
@@ -4,10 +4,17 @@
 class MapCtrl {
   constructor (){
     console.log('constructor');
+    var map = L.map('map', {
+        center: [51.505, -0.09],
+        zoom: 13
+    });
 
+    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 18
+    }).addTo(map);
+
+    var marker = L.marker([51.5, -0.09]).addTo(map);
   }
-
-
 
 
   showPopup(){
