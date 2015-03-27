bbf896cac1370754b52764fa950af9cfa0368f7f
Zukzuk <dave.timmerman@gmail.com>
2015-03-27 14:42:07 +0100

    firebase service

--- src/scripts/modules/firebase/FirebaseService.js
+++ src/scripts/modules/firebase/FirebaseService.js
@@ -2,16 +2,25 @@
 
 class FirebaseService
 {
-    constructor($firebaseArray)
-    {
-        let baseUrl = 'https://winningfrontmen.firebaseio.com/';
-        this.playersService = $firebaseArray(new Firebase(baseUrl + 'users'));
-    }
+	constructor($firebaseArray)
+	{
+		let baseUrl = 'https://winningfrontmen.firebaseio.com/';
+		this.playersService = $firebaseArray(new Firebase(baseUrl + 'users'));
+		this.markersService = $firebaseArray(new Firebase(baseUrl + 'markers'));
+	}
 
-    getPlayers()
-    {
-        return this.playersService.$loaded();
-    }
+	getPlayers()
+	{
+		return this.playersService.$loaded();
+	}
+
+	getMarkers(id)
+	{
+		if(id) {
+			return this.markersService.$getRecord(id);
+		}
+		return this.markersService.$loaded();
+	}
 }
 
 FirebaseService.$inject = ['$firebaseObject', '$firebaseArray'];
